a0525693f3248836010e35c7421c2bf1d4a250cc
GitHub Action <action@github.com>
2022-12-07 18:35:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/btbrq/simpleeditorplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/btbrq/simpleeditorplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.btbrq.simpleeditorplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.btbrq.simpleeditorplugin.MyBundle
 
 class MyApplicationService {
 
