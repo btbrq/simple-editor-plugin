dfc5386ce1c2e9a198312bba77de528577ace0ce
btbrq <brqcode@gmail.com>
2022-12-19 12:03:19 +0100

    Use proper button styles

--- src/main/kotlin/com/github/btbrq/simpleeditorplugin/popup/SimpleEditorPopup.kt
+++ src/main/kotlin/com/github/btbrq/simpleeditorplugin/popup/SimpleEditorPopup.kt
@@ -2,9 +2,12 @@ package com.github.btbrq.simpleeditorplugin.popup
 
 import com.github.btbrq.simpleeditorplugin.domain.HighlighterType
 import com.github.btbrq.simpleeditorplugin.styling.Styler
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import java.awt.Dimension
+import java.awt.Font
+import java.awt.font.TextAttribute
 import javax.swing.BoxLayout
 import javax.swing.JButton
 import javax.swing.JPanel
@@ -21,7 +24,10 @@ class SimpleEditorPopup(editor: Editor) : JPanel() {
         val dialogPanel = JPanel()
         dialogPanel.layout = BoxLayout(dialogPanel, BoxLayout.LINE_AXIS)
 
-        val jButtonColor = JButton("A")
+        val jButtonColor = JButton(AllIcons.FileTypes.Text)
+        jButtonColor.minimumSize = Dimension(40, 40)
+        jButtonColor.maximumSize = Dimension(40, 40)
+        jButtonColor.preferredSize = Dimension(40, 40)
         dialogPanel.add(jButtonColor)
         jButtonColor.addActionListener {
             JBPopupFactory.getInstance().createComponentPopupBuilder(ColorsPopup(editor, HighlighterType.COLOR), null)
@@ -29,35 +35,44 @@ class SimpleEditorPopup(editor: Editor) : JPanel() {
                 .showUnderneathOf(jButtonColor)
         }
 
-        val jButtonBackground = JButton("H")
+        val jButtonBackground = JButton(AllIcons.Actions.Highlighting)
         dialogPanel.add(jButtonBackground)
-        jButtonBackground.minimumSize = Dimension(20, 20)
-        jButtonBackground.maximumSize = Dimension(20, 20)
-        jButtonBackground.preferredSize = Dimension(20, 20)
+        jButtonBackground.minimumSize = Dimension(40, 40)
+        jButtonBackground.maximumSize = Dimension(40, 40)
+        jButtonBackground.preferredSize = Dimension(40, 40)
         jButtonBackground.addActionListener {
-            JBPopupFactory.getInstance().createComponentPopupBuilder(ColorsPopup(editor, HighlighterType.HIGHLIGHT), null)
+            JBPopupFactory.getInstance()
+                .createComponentPopupBuilder(ColorsPopup(editor, HighlighterType.HIGHLIGHT), null)
                 .createPopup()
                 .showUnderneathOf(jButtonBackground)
         }
 
         val jButton1 = JButton("U")
-        jButton1.minimumSize = Dimension(20, 20)
-        jButton1.maximumSize = Dimension(20, 20)
-        jButton1.preferredSize = Dimension(20, 20)
+        var btnFont = jButton1.font
+        val attributes = HashMap(font.attributes)
+        attributes.put(TextAttribute.SIZE, 20)
+        attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON)
+        btnFont = btnFont.deriveFont(attributes)
+        jButton1.font = btnFont
+        jButton1.minimumSize = Dimension(40, 40)
+        jButton1.maximumSize = Dimension(40, 40)
+        jButton1.preferredSize = Dimension(40, 40)
         jButton1.addActionListener { styler.underline() }
         dialogPanel.add(jButton1)
 
         val jButtonBold = JButton("B")
-        jButtonBold.minimumSize = Dimension(20, 20)
-        jButtonBold.maximumSize = Dimension(20, 20)
-        jButtonBold.preferredSize = Dimension(20, 20)
+        jButtonBold.font = Font(jButtonBold.font.name, Font.BOLD, 20)
+        jButtonBold.minimumSize = Dimension(40, 40)
+        jButtonBold.maximumSize = Dimension(40, 40)
+        jButtonBold.preferredSize = Dimension(40, 40)
         jButtonBold.addActionListener { styler.bold() }
         dialogPanel.add(jButtonBold)
 
         val jButtonItalic = JButton("I")
-        jButtonItalic.minimumSize = Dimension(20, 20)
-        jButtonItalic.maximumSize = Dimension(20, 20)
-        jButtonItalic.preferredSize = Dimension(20, 20)
+        jButtonItalic.font = Font(jButtonItalic.font.name, Font.ITALIC, 20)
+        jButtonItalic.minimumSize = Dimension(40, 40)
+        jButtonItalic.maximumSize = Dimension(40, 40)
+        jButtonItalic.preferredSize = Dimension(40, 40)
         jButtonItalic.addActionListener { styler.italic() }
         dialogPanel.add(jButtonItalic)
 
